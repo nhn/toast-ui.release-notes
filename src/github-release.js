38f9be508ca829a963697f622e6656a884983b68
홍유정/FE개발랩/NE <youjung.hong@nhnent.com>
2017-07-27 17:25:15 +0900

    refactor: github-release.js (resolves: #3) (#4)

--- src/github-release.js
+++ src/github-release.js
@@ -1,11 +1,9 @@
 'use strict';
 
-const fs = require('fs');
-const http = require('http');
 const pkg = require('../package.json');
 const Github = require('github-api');
 const {execSync} = require('child_process');
-const {argv} = process;
+const {argv, env} = process;
 
 const release = function() {
     if (argv.length < 3) {
@@ -18,111 +16,86 @@ const release = function() {
     const until = argv[3] ? argv[3] : 'HEAD';
     const CMD_GIT_LOG = `git log ${since}..${until} --pretty=format:'* %h %s (%an)'`;
 
-    const commits = execSync(CMD_GIT_LOG, {encoding: 'utf8'});
-    const releaseNote = sortByType(commits);
-    // writeReleaseNoteToFile(releaseNote);
-    postGithubRelease(releaseNote);
+    const commitLog = execSync(CMD_GIT_LOG, {encoding: 'utf8'});
+    const commitObjects = commitLog.split('\n').reduce(makeCommitObjects, []);
+    postGithubRelease(makeReleaseNote(commitObjects));
 };
 
-const commitLogRegex = /\* ([a-z0-9]{7}) (\w*):? (.*) (\(.*\))/;
-function sortByType(commits) {
-    const lines = commits.split('\n');
-    const commitArray = [];
-    lines.forEach(line => {
-        const matches = line.match(commitLogRegex);
-        const [, sha1, type, title, author] = matches;
-        const capitaledType = type[0].toUpperCase() + type.substr(1).toLowerCase();
-        const commit = {
-            sha1,
-            type: capitaledType,
-            title,
-            author
-        };
-        commitArray.push(commit);
-    });
+function makeCommitObjects(commitObjects, line) {
+    const captureGroups = getCaptureGroupsByRegex(line);
+
+    if (captureGroups) {
+        commitObjects.push(makeCommitObject(captureGroups));
+    }
+
+    return commitObjects;
+}
+
+function getCaptureGroupsByRegex(line) {
+    const commitLogRegex = /\* ([a-z0-9]{7}) (\w*):? (.*) (\(.*\))/;
+
+    return line.match(commitLogRegex);
+}
+
+function makeCommitObject(captureGroups) {
+    const [, sha1, type, title, author] = captureGroups;
+    const capitaledType = type[0].toUpperCase() + type.substr(1).toLowerCase();
+
+    return {
+        sha1,
+        type: capitaledType,
+        title,
+        author
+    };
+}
+
+function makeReleaseNote(commitObjects) {
+    const typeRegexs = [
+        /feat/i, /refactor|perf/i, /build/i, /doc/i, /fix/i
+    ];
+    const titles = [
+        'Features', 'Enhancement', 'Build Related', 'Documentation', 'Bug Fixes'
+    ];
 
     let releaseNote = '';
-    commitArray
-        .filter(commit => /feat/i.test(commit.type) || /new/i.test(commit.type))
-        .forEach((commit, index) => {
-            if (index === 0) {
-                releaseNote += '\n## Features\n\n';
-            }
-            releaseNote += renderTemplate(commit);
-        });
-    commitArray
-        .filter(commit => /update|refactor|perf/i.test(commit.type))
-        .forEach((commit, index) => {
-            if (index === 0) {
-                releaseNote += '\n## Enhancement\n\n';
-            }
-            releaseNote += renderTemplate(commit);
-        });
-    commitArray
-        .filter(commit => /build/i.test(commit.type))
-        .forEach((commit, index) => {
-            if (index === 0) {
-                releaseNote += '\n## Build Related\n\n';
-            }
-            releaseNote += renderTemplate(commit);
-        });
-    commitArray
-        .filter(commit => /doc/i.test(commit.type))
-        .forEach((commit, index) => {
-            if (index === 0) {
-                releaseNote += '\n## Documentation\n\n';
-            }
-            releaseNote += renderTemplate(commit);
-        });
-    commitArray
-        .filter(commit => /fix/i.test(commit.type))
-        .forEach((commit, index) => {
-            if (index === 0) {
-                releaseNote += '\n## Bug Fixes\n\n';
-            }
-            releaseNote += renderTemplate(commit);
-        });
+    typeRegexs.forEach((typeRegex, typeIndex) => {
+        commitObjects
+            .filter(commit => typeRegex.test(commit.type))
+            .forEach((commit, commitIndex) => {
+                releaseNote += renderTemplate(commit, commitIndex, titles[typeIndex]);
+            });
+    });
 
     return releaseNote;
 }
 
-function renderTemplate(commit) {
-    return `* ${commit.sha1} ${commit.type}: ${commit.title} ${commit.author}\n`;
-}
+function renderTemplate(commit, commitIndex, title) {
+    let releaseNote = '';
 
-function writeReleaseNoteToFile(releaseNote) {
-    const date = new Date();
-    const yyyy = date.getFullYear();
-    let MM = date.getMonth() + 1;
-    MM = MM < 10 ? '0' + MM : MM;
-    const dd = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
-    const yyyyMMdd = String(yyyy) + MM + dd;
-    fs.writeFileSync(`release-v${pkg.version}-${yyyyMMdd}`, releaseNote);
+    if (commitIndex === 0) {
+        releaseNote += `\n## ${title}\n\n`;
+    }
+    releaseNote += `* ${commit.sha1} ${commit.type}: ${commit.title} ${commit.author}\n`;
+
+    return releaseNote;
 }
 
 function postGithubRelease(releaseNote) {
+    const repoInfo = getRepositoryInfo();
 
-    if (!process.env.TUI_GITHUB_TOKEN) {
-        console.log('Missing TUI_GITHUB_TOKEN environment variable');
+    if (!hasAccessToken() || !repoInfo) {
         return;
     }
 
-    const repository = pkg.repository.split('/');
-    const length = repository.length;
-    if (length < 2) {
-        console.log('Wrong repository url on package.json');
-        return;
-    }
     const enterpriseUrl = 'https://github.nhnent.com/api/v3';
     const gh = new Github({
-        token: process.env.TUI_GITHUB_TOKEN
-    });
-    const reponame = repository[length - 1].substr(0, repository[length - 1].indexOf('.git'));
-    const repo = gh.getRepo('youjunghong', 'toast_basecamp'); //repository[length - 2], reponame);
+        token: env.TUI_GITHUB_TOKEN
+    }, enterpriseUrl);
 
+    const repo = gh.getRepo(repoInfo.username, repoInfo.reponame);
     repo.createRelease({
-        tag_name: 'REL', // `v${pkg.version}`,
-        name: '1.2.5', // `v${pkg.version}`,
+        tag_name: `v${pkg.version}`, // eslint-disable-line camelcase
+        name: `v${pkg.version}`,
         body: releaseNote,
         draft: true
     }).then(() => {
@@ -133,6 +106,34 @@ function postGithubRelease(releaseNote) {
             console.error(ex.message);
         }
     });
+}
+
+function hasAccessToken() {
+    const accessToken = env.TUI_GITHUB_TOKEN;
+    if (!accessToken) {
+        console.log('Missing TUI_GITHUB_TOKEN environment variable');
+    }
+
+    return accessToken;
+}
+
+function getRepositoryInfo() {
+    const {repository} = pkg;
+    const {length} = repository.split('/');
+
+    if (length < 2) {
+        console.log('Wrong repository url on package.json');
+        return null;
+    }
+
+    const username = repository[length - 2];
+    let reponame = repository[length - 1];
+    reponame = reponame.substr(0, reponame.indexOf('.git'));
+
+    return {
+        username,
+        reponame
+    };
 }
 
 release();
